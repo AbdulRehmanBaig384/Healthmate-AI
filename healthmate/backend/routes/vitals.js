481b88a1962d808f0c09475f1bc57c7ab579ed61
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2025-10-26 21:42:24 +0500

    Update vitals.js

--- healthmate/backend/routes/vitals.js
+++ healthmate/backend/routes/vitals.js
@@ -10,25 +10,18 @@ const {
   getVitalsSummary
 } = require('../controllers/vitalController');
 
-// All routes are protected
 router.use(protect);
 
-// Get vitals summary
 router.get('/summary', getVitalsSummary);
 
-// Get all vitals
 router.get('/', getVitals);
 
-// Add vital reading
 router.post('/', addVital);
 
-// Get single vital
 router.get('/:id', getVital);
 
-// Update vital
 router.put('/:id', updateVital);
 
-// Delete vital
 router.delete('/:id', deleteVital);
 
 module.exports = router;
