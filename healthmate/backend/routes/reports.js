952a7b725f6cc0c037c9aac13249cfefeb733ede
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2025-10-26 21:41:46 +0500

    Update reports.js

--- healthmate/backend/routes/reports.js
+++ healthmate/backend/routes/reports.js
@@ -11,25 +11,21 @@ const {
   reanalyzeReport
 } = require('../controllers/reportController');
 
-// All routes are protected
+
 router.use(protect);
 
-// Upload report with file
 router.post('/upload', upload.single('file'), handleUploadError, uploadReport);
 
-// Get all reports
 router.get('/', getReports);
 
-// Get single report
+
 router.get('/:id', getReport);
 
-// Update report
+
 router.put('/:id', updateReport);
 
-// Delete report
 router.delete('/:id', deleteReport);
 
-// Re-analyze report
 router.post('/:id/analyze', reanalyzeReport);
 
 module.exports = router;
