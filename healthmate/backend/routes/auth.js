63e0a496e8e489615bd902957bdf8c3183be89bf
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2026-05-27 21:50:32 +0500

    Update auth.js

--- healthmate/backend/routes/auth.js
+++ healthmate/backend/routes/auth.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const router=express.Router();
 const {protect}=require('../middleware/auth');
-const { register,login,getMe,updateProfile,changePassword,logout} = require('../controllers/authController');
+const {register,login,getMe,updateProfile,changePassword,logout} = require('../controllers/authController');
 
 router.post('/register', register);
 router.post('/login', login);
