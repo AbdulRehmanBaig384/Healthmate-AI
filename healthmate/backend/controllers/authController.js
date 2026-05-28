98e6706f21c8a2e32c8c72e7530b482437a82e70
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2026-05-28 20:23:58 +0500

    Refactor login function to improve readability

--- healthmate/backend/controllers/authController.js
+++ healthmate/backend/controllers/authController.js
@@ -24,7 +24,7 @@ const register=async(req,res)=>{
 };
 const login = async (req, res) => {
   try {
-    const { email, password } = req.body;
+    const { email,password}=req.body;
     if (!email || !password) {
       return res.status(400).json({
         success:false,
